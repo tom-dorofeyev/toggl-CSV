7fdbbfe3f6476f70e3fe7cead3dec0ed126f136a
tom dorofeyev <[tomdorofey@gmail.com]>
2019-08-01 08:01:53 +0300

    writing csv to file in data dir

--- api/report/report.service.js
+++ api/report/report.service.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const fs = require('fs');
 const detailedReportUrl = 'https://toggl.com/reports/api/v2/details/'
 const logger = require('../../services/logger.service')
+const { Parser } = require('json2csv');
 
 module.exports = {
     query
@@ -8,11 +10,21 @@ module.exports = {
 
 async function query(params, token) {
     try {
-        const res = await axios.get(detailedReportUrl, { 
+        const res = await axios.get(detailedReportUrl, {
             params,
             auth: { username: token, password: 'api_token' }
         })
-        return res.data
+        const data = res.data.data;
+        const fields = Object.keys(data[0]);
+        const json2csvParser = new Parser({ fields });
+        const csv = json2csvParser.parse(data);
+        fs.writeFile("data/res.csv", csv, function(err) {
+            if(err) {
+                return logger.error(err);
+            }
+            logger.info("The file was saved!");
+        }); 
+        return csv
     } catch (error) {
         logger.error('problems with query: ' + error)
         throw error
